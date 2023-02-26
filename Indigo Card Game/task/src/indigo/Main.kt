8df05c8c7740ec09bbe7c280f5acfa31e60f1838
Aaron Howser <aaronhowser1@gmail.com>
2023-02-26 13:01:56 -0600

    2/5

--- Indigo Card Game/task/src/indigo/Main.kt
+++ Indigo Card Game/task/src/indigo/Main.kt
@@ -2,15 +2,62 @@ package indigo
 
 val ranks = arrayOf("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
 val suits = arrayOf("♦", "♥", "♠", "♣")
+val deck = mutableListOf<String>()
+val hands = mutableListOf<MutableList<String>>()
 
 fun main() {
+    reset()
+    showMenu()
+}
 
-    println(ranks.joinToString(" "))
-    println(suits.joinToString(" "))
+fun showMenu() {
+    while (true) {
+        println("Choose an action (reset, shuffle, get, exit):")
+        when (readln()) {
+            "reset" -> {
+                reset()
+                println("Card deck is reset.")
+            }
+            "shuffle" -> {
+                shuffle()
+                println("Card deck is shuffled.")
+            }
+            "get" -> {
+                get()
+            }
+            "exit" -> {
+                println("Bye")
+                break
+            }
+            else -> println("Wrong action.")
+        }
+    }
+}
 
-    val deck = mutableListOf<String>()
+fun reset() {
+    deck.clear()
     for (suit in suits) for (rank in ranks) deck.add("$rank$suit")
+}
 
-    println(deck.joinToString(" "))
+fun shuffle() {
+    deck.shuffle()
+}
 
+fun get() {
+    println("Number of cards:")
+    val amount = readln()
+
+    if (amount.toIntOrNull() in 1..52) {
+
+        if (amount.toInt() <= deck.size) {
+            var hand = mutableListOf<String>()
+            for (i in 0 until amount.toInt()) {
+                hand.add(deck.first())
+                deck.removeFirst()
+            }
+            println(hand.joinToString(" "))
+        } else println("The remaining cards are insufficient to meet the request.")
+    } else {
+        println("Invalid number of cards.")
+    }
 }
