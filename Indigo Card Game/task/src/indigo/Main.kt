d273a3f52af3d13165fdc475303e2d827e39859d
Aaron Howser <aaronhowser1@gmail.com>
2023-03-06 09:21:26 -0600

    a

--- Indigo Card Game/task/src/indigo/Main.kt
+++ Indigo Card Game/task/src/indigo/Main.kt
@@ -31,8 +31,20 @@ class Deck(empty: Boolean) {
         return topCard
     }
 
+    fun removeCard(index: Int): Card {
+        val card = deck[index]
+        deck.removeAt(index)
+        return card
+    }
+
     fun add(card: Card) {
         deck.add(card)
+    }
+
+    fun takeCard(index: Int, takeFrom: Deck) {
+        if (index in 1..takeFrom.deck.size) {
+            add(takeFrom.removeCard(index))
+        }
     }
 
     fun takeCards(amount: Int, takeFrom: Deck) {
@@ -49,7 +61,12 @@ class Deck(empty: Boolean) {
 class Player(val name: String, val hand: Deck)
 
 var computersTurn = false
+var gameOver = false
 
+val deck = Deck(empty = false)
+val table = Player("Table", Deck(empty = true))
+val player = Player("Player", Deck(empty = true))
+val computer = Player("Computer", Deck(empty = true))
 
 fun main() {
 
@@ -60,11 +77,6 @@ fun main() {
 
 fun play() {
 
-    val deck = Deck(empty = false)
-    val table = Player("Table", Deck(empty = true))
-    val player = Player("Player", Deck(empty = true))
-    val computer = Player("Computer", Deck(empty = true))
-
     deck.shuffle()
 
     table.hand.takeCards(4, deck)
@@ -74,14 +86,61 @@ fun play() {
     println("Initial cards on the table: ${table.hand.deck.joinToString(" ")}")
 
 
+    while (!gameOver) {
+        playTurn()
+    }
+
+    println("Game Over")
 }
 
 fun playTurn() {
+    println("\n${table.hand.deck.size} cards on the table, and the top card is ${topCard(table.hand)}")
+    val currentPlayer = if (computersTurn) computer else player
 
+    if (currentPlayer.hand.deck.isEmpty()) currentPlayer.hand.takeCards(6, deck)
+    if (computersTurn) computersTurn() else playersTurn()
 
     computersTurn = !computersTurn
 }
 
+fun playersTurn() {
+
+    print("Cards in hand:")
+    for (i in 0 until player.hand.deck.size) {
+        print(" ${i+1})${player.hand.deck[i]}")
+    }
+    print("\n")
+
+    val deckSize = player.hand.deck.size
+
+    val pickedCardIndex: Int
+
+    while (true) {
+        val cardNumber = inputFromPrompt("Choose a card to play: (1-${deckSize})")
+
+        if (cardNumber.lowercase() == "exit") {
+            gameOver = true
+            return
+        }
+
+        if (cardNumber.toIntOrNull() in 1..deckSize) {
+            pickedCardIndex = cardNumber.toInt() - 1
+            break
+        }
+    }
+
+    //TODO: figure out why this isn't taking index 0 cards
+    table.hand.takeCard(pickedCardIndex, player.hand)
+
+}
+
+fun computersTurn() {
+
+}
+
+fun topCard(deck: Deck): Card {
+    return deck.deck.last()
+}
 
 fun playFirstCheck() {
     when (inputFromPrompt("Play first?").lowercase()) {
